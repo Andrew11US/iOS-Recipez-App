dcca2bedeaae23a9550fc03c93e8b4191880c0bd
Andrew Foster <AndrewFoster@MacBook-AF.local>
2016-09-30 20:47:16 +0200

    Initial Commit

---
+++ Recipez/RecipeCell.swift
@@ -0,0 +1,27 @@
+//
+//  RecipeCell.swift
+//  Recipez
+//
+//  Created by Andrew Foster on 9/28/16.
+//  Copyright © 2016 Foster Inc. All rights reserved.
+//
+
+import UIKit
+
+class RecipeCell: UITableViewCell {
+
+    @IBOutlet weak var recipeTitle: UILabel!
+    @IBOutlet weak var recipeImg: UIImageView!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    func configureCell(recipe: Recipe) {
+        recipeTitle.text = recipe.title
+        recipeImg.image = recipe.getRecipeImg()
+    }
+
+}
